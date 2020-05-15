a7a5f06f6c893a2182017131f1f1eb8e946a8c3d
Marco Jeffrey Pansa <51530201+MJPansa@users.noreply.github.com>
2020-05-15 21:14:49 +0200

    Add files via upload

---
+++ train.py
@@ -0,0 +1,66 @@
+import numpy as np
+import torch as T
+from unityagents import UnityEnvironment
+from utils import ActorCritic, play_episode, calculate_g, train_on_episode, play_test_episode
+import wandb
+
+wandb.init(project="actor_critic_continuous")
+
+env = UnityEnvironment(
+    file_name='Reacher_Linux/Reacher.x86_64', no_graphics=True)
+
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+env_info = env.reset(train_mode=True)[brain_name]
+states = env_info.vector_observations
+
+N_STATES = states.shape[1]
+N_ACTIONS = brain.vector_action_space_size
+N_HIDDEN = 128
+GAMMA = 0.95
+LR = 1e-4
+CRITIC_WEIGHT = 1
+DEVICE = 'cuda:1'
+ENTROPY_BETA = 0.0001
+N_STEPS = 5
+
+
+model = ActorCritic(n_states=N_STATES, n_actions=N_ACTIONS, n_hidden=N_HIDDEN, lr=LR)
+model = model.to(DEVICE)
+
+#wandb.watch(model)
+
+run = 1
+rewards_total = []
+
+while True:
+    states_list, rewards_list, actions_list, values_list = play_episode(env, brain_name, model, DEVICE, N_STEPS)
+    G_list = calculate_g(rewards_list, values_list, GAMMA)
+    actor_loss, critic_loss, loss, entropy_loss, mean_sigma = train_on_episode(model,
+                                                                               states_list, actions_list, G_list,
+                                                                               LR, CRITIC_WEIGHT, DEVICE, ENTROPY_BETA)
+
+    run += 1
+
+    if run % 5 == 0:
+        mean_rewards = play_test_episode(env, brain_name, model, DEVICE)
+        wandb.log({'rewards': mean_rewards,
+                   'actor_loss': actor_loss,
+                    'critic_loss': critic_loss,
+                   'loss': loss,
+                   'entropy_loss': entropy_loss,
+                   'mean_sigma': mean_sigma})
+        rewards_total.append(mean_rewards)
+        print(f'Finished run {run}\nmean-rewards: {np.mean(rewards_total[-10:]):.2f}\nLast test-run: {mean_rewards:.2f}\n')
+
+        if np.mean(rewards_total[-10:]) > 31.:
+            print(f'SOLVED AFTER {run} n-step updates')
+            break
+
+    if run % 1000 == 0:
+        T.save(model, f'a2c_multi_reacher_run_{run}_{mean_rewards:.2}.h5')
+        T.save(model.state_dict(), f'a2c_multi_reacher_run_{run}_{mean_rewards:.2}_dict.h5')
+
+T.save(model, f'a2c_multi_reacher_run_{run}_solved.h5')
+T.save(model.state_dict(), f'a2c_multi_reacher_run_{run}_solved_dict.h5')
+
---
+++ utils.py
@@ -0,0 +1,136 @@
+import torch as T
+import torch.optim as optim
+import torch.nn.modules as nn
+from torch.nn.utils.clip_grad import clip_grad_norm_
+import torch.nn.functional as F
+import torch.distributions as dist
+import numpy as np
+import sys
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, n_states, n_actions, n_hidden, lr):
+        super(ActorCritic, self).__init__()
+
+        self.input = nn.Linear(n_states, n_hidden)
+        self.hidden = nn.Linear(n_hidden, n_hidden)
+        self.out_actor_sigma = nn.Linear(n_hidden, n_actions)
+        self.out_actor_mu = nn.Linear(n_hidden, n_actions)
+        self.out_critic = nn.Sequential(nn.Linear(n_hidden, n_hidden),
+                                        nn.ReLU(), nn.Linear(n_hidden, 1))
+
+        #self.bn_1 = nn.BatchNorm1d(n_hidden)
+        #self.bn_2 = nn.BatchNorm1d(n_hidden)
+
+        self.optimizer = optim.SGD(self.parameters(), lr=lr)
+
+    def forward(self, x):
+        if not self.training:
+            x = x.float()
+
+        x = F.relu(self.input(x))
+        x = F.relu(self.hidden(x))
+
+        mus = F.tanh(self.out_actor_mu(x))
+        sigmas = F.relu(self.out_actor_sigma(x))
+        sigmas = T.clamp(sigmas, 5e-4, 2)
+        value = self.out_critic(x.detach())
+
+        return mus, sigmas, value.squeeze()
+
+
+def play_episode(env, brain_name, model, device, n_steps):
+    states_list = []
+    rewards_list = []
+    actions_list = []
+    values_list = []
+    model.eval()
+
+    env_info = env.reset(train_mode=True)[brain_name]
+    states = env_info.vector_observations
+    t = 0
+    while t < n_steps:
+        t += 1
+        mus, sigmas, values = model(T.from_numpy(states).to(device))
+        distribution = dist.Normal(mus, sigmas)
+        actions = distribution.sample()
+        actions = T.clamp(actions, -1, 1)
+        env_info = env.step(actions.squeeze().detach().cpu().numpy())[brain_name]
+        next_states = env_info.vector_observations
+        rewards = env_info.rewards
+        dones = env_info.local_done
+        values_list.append(values)
+        states_list.append(T.from_numpy(states).to(device))
+        rewards_list.append(rewards)
+        actions_list.append(actions.squeeze())
+        states = next_states.copy()
+        if np.any(dones):
+            break
+    return states_list, rewards_list, actions_list, values_list
+
+
+def calculate_g(rewards_list, values_list, gamma):
+    rewards = np.flip(np.stack(rewards_list), axis=0)
+    last_states = values_list[-1].detach().cpu().numpy()
+    for i in range(rewards.shape[0]):
+        if i == 0:
+            rewards[i, :] = rewards[i, :] + gamma * last_states
+        else:
+            rewards[i, :] = rewards[i, :] + gamma * rewards[i - 1, :]
+    return np.flip(rewards)
+
+
+def train_on_episode(model, states_list, action_list, rewards_list, lr, critic_weight, device, entropy_beta):
+    model.train()
+
+    actions_tensor = T.stack(action_list, dim=0)
+    states = T.stack(states_list)
+    states = T.stack([states[:, n, :] for n in range(20)], dim=0)
+    states = states.view(-1, states.size(-1))
+    actions = T.stack([actions_tensor[:, n, :] for n in range(20)], dim=0)
+    actions = actions.view(-1, actions.size(-1))
+    rewards = T.stack([T.Tensor(rewards_list[:, n]) for n in range(20)], dim=0).to(device)
+    rewards = rewards.view(-1)
+
+    rewards = F.normalize(rewards, dim=0)
+
+    mus, sigmas, state_values = model(states.float())
+    gaussian = dist.Normal(mus, sigmas)
+    logprobs = gaussian.log_prob(actions.float().to(device)).squeeze().float().mean(1)
+    entropy = gaussian.entropy().squeeze().float()
+    entropy_loss = (-1 * entropy.mean(1) * entropy_beta).sum()
+    actor_loss = (-1 * (logprobs * (rewards.float() - state_values.squeeze().detach().float()))).sum() + entropy_loss
+    critic_loss = (T.pow((rewards - state_values.squeeze()), 2)).sum()
+    loss = actor_loss + (critic_weight * critic_loss)
+
+    model.optimizer.zero_grad()
+    loss.backward()
+    clip_grad_norm_(model.parameters(), 5.)
+    model.optimizer.step()
+
+    mean_sigma = sigmas.mean()
+
+    return [x.detach().cpu().item() for x in [actor_loss, critic_loss, loss, entropy_loss, mean_sigma]]
+
+
+def play_test_episode(env, brain_name, model, device):
+    rewards_list = []
+    model.eval()
+    done = False
+    env_info = env.reset(train_mode=True)[brain_name]
+    states = env_info.vector_observations
+    while not done:
+        mus, sigmas, values = model(T.from_numpy(states).to(device))
+        distribution = dist.Normal(mus, sigmas)
+        actions = distribution.sample()
+        actions = T.clamp(actions, -1, 1)
+
+        env_info = env.step(actions.squeeze().detach().cpu().numpy())[brain_name]
+        next_states = env_info.vector_observations
+        rewards = env_info.rewards
+        dones = env_info.local_done
+        rewards_list.append(np.mean(rewards))
+        states = next_states.copy()
+        if np.any(dones):
+            done = True
+    return sum(rewards_list)
